02f902d55f9d955f4e2ec2ae39fadd4b0cfd3dcf
bfasenfest <aowbackup@gmail.com>
2023-02-27 11:46:42 -0500

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src")
     }
   },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
+  },
   optimizeDeps: {
     esbuildOptions: {
       plugins: [
